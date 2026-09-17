388e81cca4d756f02e1605184b51d9bd26b496ea
Dzulfan Fadli <dzulfan.dev@gmail.com>
2026-09-17 20:06:56 +0700

    Day 7: Arrays

---
+++ day-7-arrays.js
@@ -0,0 +1,7 @@
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+  console.log(arr.reverse().join(' '));
+}
